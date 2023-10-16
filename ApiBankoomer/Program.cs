37326294178cf7d757bd65482d12c906b899bd1e
Bass <al233324@edu.uaa.mx>
2023-10-15 21:22:14 -0500

    ConnectionString añadida

--- ApiBankoomer/Program.cs
+++ ApiBankoomer/Program.cs
@@ -1,3 +1,6 @@
+using MySQLConnectorData;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Conección a la base de datos, es un singleton para manejar una sola conección para toda la base de datos.
+builder.Services.AddSingleton(new MySQLConfig(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Cors con politicas que permiten cualquier cosa, se puede cambiar para que solo permita ciertas conecciones.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
